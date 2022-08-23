e7fa939b060a320cfcafa9b303ef85700465f347
saniok333 <alex.o.csc@gmail.com>
2022-08-23 18:06:32 +0300

    Working on Expense List Items

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,8 +1,16 @@
 import { FlatList, Text } from 'react-native';
+import ExpenseItem from './ExpenseItem';
 
-const renderExpenseItem = ({ item: { description, amount, date } }) => (
-  <Text>{description}</Text>
-);
+const renderExpenseItem = ({ item: { id, description, amount, date } }) => {
+  const expenseItemProps = {
+    id,
+    description,
+    amount,
+    date,
+  };
+
+  return <ExpenseItem {...expenseItemProps} />;
+};
 
 const ExpensesList = ({ expenses }) => {
   return (
