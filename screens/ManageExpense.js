ae2fc9d371f832c9cb75a8a94992ebbf1ef28060
saniok333 <alex.o.csc@gmail.com>
2022-08-24 13:45:44 +0300

    Adding a "Delete" Button

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../constants/styles';
 
 const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -11,7 +13,37 @@ const ManageExpense = ({ route, navigation }) => {
     });
   }, [navigation, isEditing]);
 
-  return <Text>ManageExpense</Text>;
+  const deleteExpenseHandler = () => {};
+
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            size={36}
+            color={GlobalStyles.colors.error500}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 };
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
