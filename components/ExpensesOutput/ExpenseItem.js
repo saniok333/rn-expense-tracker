50ba2a4dace3161c285befc6737b76fb3f03d1a8
saniok333 <alex.o.csc@gmail.com>
2022-08-24 13:29:29 +0300

    Supporting Different Editing Modes & Using Route Parameters

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -4,10 +4,10 @@ import { useNavigation } from '@react-navigation/native';
 import { GlobalStyles } from '../../constants/styles';
 import { getFormattedDate } from '../../util/date';
 
-const ExpenseItem = ({ description, amount, date }) => {
+const ExpenseItem = ({ id, description, amount, date }) => {
   const navigation = useNavigation();
   const expensePressHandler = () => {
-    navigation.navigate('ManageExpense');
+    navigation.navigate('ManageExpense', { expenseId: id });
   };
   return (
     <Pressable
