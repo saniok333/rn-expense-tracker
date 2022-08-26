835f0aa278c9966d659f94f7f366e93b21bbb02b
saniok333 <alex.o.csc@gmail.com>
2022-08-26 14:00:47 +0300

    Fetching Backend Data (GET Requests)

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,11 +1,21 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/date';
+import { fetchExpenses } from '../util/http';
 
 const RecentExpenses = () => {
   const { expenses } = useContext(ExpensesContext);
+
+  useEffect(() => {
+    const getExpenses = async () => {
+      const expenses = await fetchExpenses();
+    };
+
+    getExpenses();
+  }, []);
+
   const recentExpenses = expenses.filter(
     (expense) =>
       expense.date >= getDateMinusDays(7) && expense.date <= new Date()
